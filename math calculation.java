b93fde8525fbb40bd3d39edd1d43f8fa314852c2
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-09-07 11:08:28 +0530

    Add files via upload

---
+++ math calculation.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    int x,y,z;
+    x=6;
+    y=4;
+    z=x-y;
+    System.out.println("substract two integers"+z);
+    int a,b,c;
+    a=3;
+    b=4;
+    c=a+b; 
+    System.out.println("addition two integers"+c);
+    int p,q,r;
+    p=7;
+    q=5;
+    r=p*q;
+    System.out.println("multiply two integers"+r);
+    int d,e,f;
+    d=9;
+    e=9;
+    f=d/e;
+    System.out.println("division two integers"+f);
+  }
+}
