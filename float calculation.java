1a9f7291e06dfd7c16cf62dcd0daa98588aa7e11
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-09-07 11:39:56 +0530

    Add files via upload

---
+++ float calculation.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    float x,y,z;
+    x=6.2f;
+    y=4.1f;
+    z=x-y;
+    System.out.println("substract two floats"+z);
+    int a,b,c;
+    a=3;
+    b=4;
+    c=a+b; 
+    System.out.println("addition two integers"+c);
+    float p,q,r;
+    p=7.9f;
+    q=5.5f;
+    r=p*q;
+    System.out.println("multiply two floats"+r);
+    int d,e,f;
+    d=9;
+    e=9;
+    f=d/e;
+    System.out.println("division two integers"+f);
+  }
+}
