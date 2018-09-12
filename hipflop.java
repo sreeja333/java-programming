805bf17cdd61b2eb90c727fd8fe47243f16cd03a
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-09-12 15:48:25 +0530

    Add files via upload

---
+++ hipflop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+	public static void main (String[] args) {
+      int dayslntheater = 50;
+      if(dayslntheater<=50);
+      System.out.println("Box Office Hit");
+      else
+      System.out.println("Box Office Flop");
+	}
+}
