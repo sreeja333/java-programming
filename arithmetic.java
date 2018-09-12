7d54c37410f42d2aae0fd5ffec22079fa7e4a52c
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-09-12 15:15:21 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class main
+{
+	public static void main (String[] args){
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t;
+      a=40;
+      b=20;
+     c=a+b;
+     d=a-b;
+     e=a*b;
+     f=b/a;
+     g=b%a;
+     i=b++;
+     j=b—;
+     k=a&b;
+     l=a|b;
+     m=a^b;
+     n=~a;
+     o=a<<2;
+     p=>>2;
+     q=a>>>2;
+     r=a&&b;
+     s=a||b;
+     t=!(a&&b);
+      System.out.println("output is"+c);
+      System.out.println("output is"+d);
+      System.out.println("output is"+e);
+      System.out.println("output is"+f);
+      System.out.println("output is"+g);
+      System.out.println("output is"+h);
+      System.out.println("output is"+i);
+      System.out.println("output is"+j);
+      System.out.println("output is"+k);
+      System.out.println("output is"+l);
+      System.out.println("output is"+m);
+      System.out.println("output is"+n);
+      System.out.println("output is"+o);
+      System.out.println("output is"+p);
+      System.out.println("output is"+q);
+      System.out.println("output is"+r);
+      System.out.println("output is"+s);
+      System.out.println("output is"+t);
+    }
+}
