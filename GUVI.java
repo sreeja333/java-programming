71be444443e6e9a0c56d0376fd3f734aa154a682
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-10-04 12:13:07 +0530

    Update GUVI.java

--- GUVI.java
+++ GUVI.java
@@ -4,7 +4,7 @@ import java.io.*;
 class GUVI {
    public static void main(String args[] ){
    Scanner reader = new Scanner(System.in);
-   System.out.println("enter the number");
+   //System.out.println("enter the number");
    int num=reader.nextInt();
    if (num% 2 == 0)
    System.out.println("the number is even.");
