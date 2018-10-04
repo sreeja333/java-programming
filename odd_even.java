22d32592e7c3ff9cd0f131c49702cf1a7be12ae4
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-10-04 12:32:37 +0530

    Update and rename GUVI.java to odd_even.java

--- odd_even.java
+++ odd_even.java
@@ -1,12 +1,11 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class GUVI {
+class odd_even {
    public static void main(String args[] ){
-   Scanner reader = new Scanner(System.in);
-   //System.out.println("enter the number");
-   int num=reader.nextInt();
-   if (num% 2 == 0)
+   Scanner reader = new Scanner(System.in)
+   int number=reader.nextInt();
+   if (number% 2 == 0)
    System.out.println(" even.");
    else
    System.out.println("odd.");
