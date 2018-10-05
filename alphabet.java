5865f7c9db29f87f4a39f83785236bc1181d7040
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-10-05 12:13:25 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,9 @@
+class alphabet {
+public static void main(String[] args) {
+char c = 'd';
+if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+System.out.println(c + " Alphabet.");
+else
+System.out.println(c + " Alphabet.");
+}
+}
