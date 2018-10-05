af05b72bb226d8fb5463e2f4af897738b71b7837
sreeja333 <43057367+sreeja333@users.noreply.github.com>
2018-10-05 12:25:46 +0530

    Add files via upload

---
+++ largestnum.java
@@ -0,0 +1,11 @@
+class largestnum {
+public static void main(String[] args) {
+double n1 = 1, n2 = 1, n3 = -4;
+if( n1 >= n2 && n1 >= n3)
+System.out.println(n1 + " 1 ");
+else if (n2 >= n1 && n2 >= n3)
+System.out.println(n2 + " 1 ");
+else
+System.out.println(n3 + " -4 ");
+}
+}
